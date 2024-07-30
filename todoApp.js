de1eda6f8e069ad9f61c406a67ce3904d2c196c9
24LU1697@nest.edu.mn <24LU1697@G302-CC0111.local>
2024-07-30 12:40:35 +0800

    fix checkbutton

--- todoApp.js
+++ todoApp.js
@@ -7,7 +7,7 @@ const board = [
 const boards = document.getElementById("boardss");
 
 board.forEach((item) => {
-  boards.innerHTML += `<div class="board">
+  boards.innerHTML += `<div class="board" id="${item.class}">
             <div class="board-header"><div id="color" style="background-color:${item.title} ;" ></div>${item.name}</div>
             <div class="cards ${item.class}" id="${item.id}">
             
@@ -35,6 +35,10 @@ const helper2 = document.querySelector(".helper-text2");
 const statusSelect = document.getElementById("Status");
 const prioritySelect = document.getElementById("Priority");
 
+const boardDrop = document.querySelector(".board");
+let uid = 1;
+let cid = 1000;
+
 const values = {
   title: "",
   desc: "",
@@ -43,10 +47,12 @@ const values = {
   id: "",
 };
 
-const createNewTask = () => {
-  return ` <div class="card" draggable="true" id=${values.id}>
-                <div class="done">
+const createNewTask = (check, checkId) => {
+  return ` <div class="card" draggable="true" id=${values.id} ondragstart="dragDrop1(event)">
+                <div id=${checkId}>
+                <button class=${check} onclick="move(event)">
                   <i class="fa fa-check icon" aria-hidden="true"></i>
+                </button>
                 </div>
                 <div class="details">
                 <h4>${values.title}</h4>
@@ -68,10 +74,30 @@ const createNewTask = () => {
 };
 const remove = (event) => {
   const deleteCard = document.getElementById(
-    event.target.parentNode.parentNode.id
+    event.currentTarget.parentNode.parentNode.id
   );
   const parent = document.getElementById(deleteCard.parentNode.id);
   parent.removeChild(deleteCard);
+};
+
+const move = (event) => {
+  const checkButton = document.getElementById(
+    event.currentTarget.parentNode.id
+  );
+  if (
+    document.getElementById(
+      event.currentTarget.parentNode.parentNode.parentNode.id
+    ) !== doneContainer
+  ) {
+    const deleteCard = document.getElementById(
+      event.currentTarget.parentNode.parentNode.id
+    );
+    doneContainer.appendChild(deleteCard);
+
+    checkButton.innerHTML = `<button class="checked" onclick="move(event)">
+    <i class="fa fa-check icon" aria-hidden="true"></i>
+  </button>`;
+  }
 };
 
 addBtn.forEach((el) => {
@@ -84,36 +110,28 @@ away.addEventListener("click", (event) => {
   helper1.style.display = "none";
   helper2.style.display = "none";
 });
-
+const addTask = (value, doneCheck) => {
+  const container = document.getElementById(value);
+  if (container == doneContainer) {
+    doneCheck = "checked";
+  }
+  container.innerHTML += createNewTask(doneCheck, cid);
+  uid += 1;
+  cid += 1;
+};
 taskbtn.addEventListener("click", (event) => {
   values.title = title.value;
   values.desc = Description.value;
   values.status = statusSelect.value;
   values.priority = prioritySelect.value;
-
+  values.id = uid;
   if (title.value.length !== 0 && Description.value.length !== 0) {
-    if (statusSelect.value === "InProgress") {
-      console.log(cards);
-      let uid = 123;
-      values.id = uid;
-      processContainer.innerHTML += createNewTask();
-    } else if (statusSelect.value === "Done") {
-      let uid = 1;
-      values.id = uid;
-      doneContainer.innerHTML += createNewTask();
-    } else if (statusSelect.value === "Todo") {
-      let uid = 1;
-      values.id = uid;
-      todoContainer.innerHTML += createNewTask();
-    } else if (statusSelect.value === "Blocked") {
-      let uid = 1;
-      values.id = uid;
-      blockedContainer.innerHTML += createNewTask();
-    }
+    let doneCheck = "checkdone";
+    addTask(statusSelect.value, doneCheck);
     taskbox.style.display = "none";
     title.value = "";
     Description.value = "";
-    statusSelect.value = "Todo";
+    statusSelect.value = "todo";
     prioritySelect.value = "Low";
     helper1.style.display = "none";
     helper2.style.display = "none";
@@ -122,3 +140,54 @@ taskbtn.addEventListener("click", (event) => {
     helper2.style.display = "block";
   }
 });
+
+const dragDrop1 = (event) => {
+  event.dataTransfer.setData(
+    "text/plain",
+    JSON.stringify({
+      childId: event.target.id,
+      parentId: event.target.parentElement.id,
+    })
+  );
+};
+
+const dragDrop2 = (event) => {
+  // const itemId = JSON.parse(event.dataTransfer.getData("text/plain"));
+  console.log(event.currentTarget.id);
+};
+
+// const dragDrop2 = (event, id) => {
+//   event.preventDefault();
+//   event.addEventListener("drop", (el) => {
+//     const itemId = JSON.parse(event.dataTransfer.getData("text/plain"));
+//     if (!inner.children.length) {
+//       event.target.appendChild(document.getElementById(itemId.childId));
+//     } else {
+//       const existingElement = document.getElementById(event.target.id);
+//       inner.removeChild(existingElement);
+//       inner.appendChild(document.getElementById(itemId.childId));
+//       const parentid = itemId.parentId;
+//       const parentElement = document.getElementById(parentid);
+//       parentElement.appendChild(existingElement);
+//     }
+//   });
+// };
+
+// boardDrop.forEach((inner, id) => {
+// inner.addEventListener("dragover", (event) => {
+//   event.preventDefault();
+// });
+// inner.addEventListener("drop", (event) => {
+// const itemId = JSON.parse(event.dataTransfer.getData("text/plain"));
+// if (!inner.children.length) {
+//   event.target.appendChild(document.getElementById(itemId.childId));
+// } else {
+//   const existingElement = document.getElementById(event.target.id);
+//   inner.removeChild(existingElement);
+//   inner.appendChild(document.getElementById(itemId.childId));
+//   const parentid = itemId.parentId;
+//   const parentElement = document.getElementById(parentid);
+//   parentElement.appendChild(existingElement);
+// }
+// });
+// });
